bd224d200bd81708ef4f79f108b68dcbb320fbe6
PierreJacobs <pierre.jacobs@student.unamur.be>
2021-10-22 11:27:31 +0200

    SWIPE DONE

--- Assets/AHMI/Scripts/ChangeArms.cs
+++ Assets/AHMI/Scripts/ChangeArms.cs
@@ -1,51 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Leap;
 
 public class ChangeArms : MonoBehaviour
 {
 
     bool bIsLeftPalmUp;
-    bool bHasRightPalmBeenNorthWest;
-    bool bIsRightPalmNotNorthWest;
+    public int update_rate;
+    public int xVelocity;
+    int tick;
+
+    Controller controller;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        // init variables
         this.bIsLeftPalmUp = false;
-        this.bHasRightPalmBeenNorthWest = false;
-        this.bIsRightPalmNotNorthWest = false;
+        controller = new Controller();
+        tick = update_rate;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        Frame fFrame = controller.Frame ();
+        Hand hRightHand = null;
+
+        if (fFrame.Hands.Count > 0){
+            List<Hand> hands = fFrame.Hands;
+            foreach (Hand hand in hands) if (hand.IsRight) hRightHand = hand;
+        }
+
+        if (hRightHand != null && this.bIsLeftPalmUp && tick <= 0 && hRightHand.PalmNormal.x < -0.5) {
+            
+            if (hRightHand.PalmVelocity.x < -xVelocity) { 
+                print("Move Done!!! To the left"); 
+                tick = update_rate; 
+                //TODO: Interact with robot
+                }
+
+            else if (hRightHand.PalmVelocity.x > xVelocity) {
+                 print("Move Done!!! To the right"); 
+                 tick = update_rate; 
+                //TODO: Interact with robot
+            }
+        }
+
+        tick--;
+
     }
 
     public void LeftPalmUp() { this.bIsLeftPalmUp = true; }
     public void LeftPalmNotUp() { this.bIsLeftPalmUp = false; }
-
-    public void RightPalmNorthWest() { 
-        if (this.bIsLeftPalmUp) { 
-            this.bHasRightPalmBeenNorthWest = true; 
-            this.bIsRightPalmNotNorthWest = false;
-        }
-    }
-
-    public void RightPalmNotNorthWest() { 
-        this.bIsRightPalmNotNorthWest = true; 
-    }
-
-    public void RightPalmSouthWest() {
-        if (this.bIsLeftPalmUp && this.bHasRightPalmBeenNorthWest && this.bIsRightPalmNotNorthWest) {
-            this.bHasRightPalmBeenNorthWest = false;
-
-            //TODO: Make the robot change arm
-            print("MOVE DONE");
-        }
-    }
 
 }
