773536d4b85315d9109005204d6a39d124307e4a
PierreJacobs <pierre.jacobs@student.unamur.be>
2021-10-29 13:18:15 +0200

    ChangeArmsGesture: added trigger condition in processGestures()

--- Assets/AHMI/Scripts/ChangeArmsGesture.cs
+++ Assets/AHMI/Scripts/ChangeArmsGesture.cs
@@ -26,10 +26,22 @@ public class ChangeArmsGesture : Gesture
     }
 
     protected new bool checkRightHand() {
-        return fElaspedTime <= 0 && this.getRightHand().PalmNormal.x < -0.5;  
+        return /* fElaspedTime <= 0 && */ this.getRightHand().PalmNormal.x < -0.5;  
     }
 
     protected override void processGestures() {
+
+        /*
+        Problèmes ici (les conditions viennent de ChangeArm.cs):
+        * on ne check pas fElaspedTime <= 0, donc il n'y a aucun cooldown et c'est pour ça que le robot tournait plusieurs fois
+        * on ne check pas this.PalmsOrientationScript.IsLeftPalmUp(), donc on ne prend même pas en compte le main gauche (alors qu'on devrait)
+        * on ne check pas hRightHand.PalmNormal.x < -0.5, donc on ne prend même pas en compte l'orientation de la main droite
+        De ce que je vois, les fonctions checkLeftHand() et checkRightHand() s'occupent de ça, mais ne sont pas appelées.
+        */
+
+        //Ce que je rajoute: (avec ça ça devrait marcher normalement)
+        if (!this.checkLeftHand() || !this.checkRightHand() || this.fElaspedTime > 0) return;
+
         if (this.getRightHand().PalmVelocity.x < -xVelocity) { 
                 fElaspedTime = fRestTime;
                 this.GetRobot().TurnLeft();
