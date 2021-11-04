616e311b181bdaab59f8d83fcff5a5b6624377d9
Goblin-hub <56274669+Goblin-hub@users.noreply.github.com>
2021-11-04 11:14:10 +0100

    RotationGesture first attempt. Reorder Arms to match the current rotation system.

--- Assets/AHMI/Scripts/RobotBehaviours.cs
+++ Assets/AHMI/Scripts/RobotBehaviours.cs
@@ -7,7 +7,7 @@ using System;
 public class RobotBehaviours : MonoBehaviour
 {
 
-    public enum Arms { Hammer, Unused, Clamp, Welder }
+    public enum Arms { Clamp, Welder, Hammer, Unused }
     public int currentHandQuadrant = 0;
 
     public Text tTextElement;
@@ -23,16 +23,20 @@ public class RobotBehaviours : MonoBehaviour
     public GameObject circleObject;
     public float RotationSpeed;
 
+    public Quaternion robotRotation;
+    public float robotRotationSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        robotRotation = this.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
         tTextElement.text = "Arm: " + Enum.GetName(typeof(Arms), getArm());
-        circleObject.transform.rotation = Quaternion.Slerp(circleObject.transform.rotation, Quaternion.Euler(0, this.currentHandQuadrant*90, 0), Time.deltaTime * RotationSpeed);
+        circleObject.transform.localRotation  = Quaternion.Slerp(circleObject.transform.localRotation , Quaternion.Euler(0, this.currentHandQuadrant*90, 0), Time.deltaTime * RotationSpeed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, this.robotRotation, Time.deltaTime * robotRotationSpeed);
     }
 }
---
+++ Assets/AHMI/Scripts/RotationGesture.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+
+public class RotationGesture : Gesture
+{
+
+    public GameObject point;
+    public float minRadius;
+    public float maxRadius;
+    protected override bool needLeftHand() { return false; }
+    protected override bool needRightHand() { return true; }
+    protected override bool checkRightHand() { 
+        float distance = Vector3.Distance(unityVector(this.getRightHand().PalmPosition), point.transform.position);
+        return distance >= minRadius && distance <= maxRadius;
+    }
+
+    protected override void processGestures() {
+        Vector3 rotationVector = unityVector(this.getRightHand().PalmPosition) - point.transform.position;
+        this.GetRobot().robotRotation *= Quaternion.Euler(rotationVector*0.2f);   
+    }
+
+    protected override void processOthers() { return; }
+
+
+    private Vector3 unityVector(Leap.Vector vector) {
+        return new Vector3(vector.x, vector.y, vector.z);
+    }
+}
